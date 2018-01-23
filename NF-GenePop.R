91cecf572a040f6ad47252bd51c6995d3d0ac102
laurahspencer <lhs3@uw.edu>
2018-01-22 21:49:43 -0800

    knitted r markdown file

--- NF-GenePop.R
+++ NF-GenePop.R
@@ -2,17 +2,19 @@
 
 install.packages("genepop")
 library(genepop)
-basic_info("../Data/Oly2016NFH+2017NFW_Merged.txt", verbose=T)
 
-genedivFis(inputFile="../Data/Oly2016NFH+2017NFW_Merged.txt", sizes=FALSE, outputFile = "../Analyses/NF-Fis.txt", dataType = "Diploid", verbose=interactive())
+setwd("~/Documents/Roberts Lab/O.lurida_genetics")
 
-test_HW(inputFile = "../Data/Oly2016NFH+2017NFW_Merged.txt", which="Proba", outputFile = "../Analyses/NF-HWE.txt", enumeration = FALSE, dememorization = 10000, batches = 100,
-        iterations = 1000, verbose = interactive())
+basic_info(inputFile="Data/Oly2016NFH+2017NFW_Merged.txt", outputFile = "Analyses/NF-Info.txt", verbose=T)
 
-test_LD(inputFile = "../Data/Oly2016NFH+2017NFW_Merged.txt", outputFile = "../Analyses/NF-LD.txt", dememorization = 10000, batches = 100, iterations = 1000, verbose = TRUE)
+test_HW(inputFile = "Data/Oly2016NFH+2017NFW_Merged.txt", which="Proba", outputFile = "Analyses/NF-HWE.txt", enumeration = FALSE, dememorization = 10000, batches = 100, iterations = 1000, verbose = interactive())
 
-nulls(inputFile = "../Data/Oly2016NFH+2017NFW_Merged.txt", outputFile = "../Analyses/NF-null.txt", nullAlleleMethod = "B96", CIcoverage = 0.95, verbose = TRUE)
+test_LD(inputFile = "Data/Oly2016NFH+2017NFW_Merged.txt", outputFile = "Analyses/NF-LD.txt", dememorization = 10000, batches = 100, iterations = 1000, verbose = TRUE)
 
-Fst(inputFile = "../Data/Oly2016NFH+2017NFW_Merged.txt", outputFile = "../Analyses/NF-Fst.txt", sizes=F, pairs=TRUE, dataType="Diploid", verbose = TRUE)
+nulls(inputFile = "Data/Oly2016NFH+2017NFW_Merged.txt", outputFile = "Analyses/NF-null.txt", nullAlleleMethod = "B96", CIcoverage = 0.95, verbose = TRUE)
 
-test_diff(inputFile = "../Data/Oly2016NFH+2017NFW_Merged.txt", outputFile = "../Analyses/NF-Diff.txt", genic=FALSE, pairs=TRUE, dememorization = 10000, batches = 100, iterations = 1000, verbose = TRUE)
+test_diff(inputFile = "Data/Oly2016NFH+2017NFW_Merged.txt", outputFile = "Analyses/NF-Diff.txt", genic=FALSE, pairs=TRUE, dememorization = 10000, batches = 100, iterations = 1000, verbose = TRUE)
+
+Fst(inputFile = "Data/Oly2016NFH+2017NFW_Merged.txt", outputFile = "Analyses/NF-Fst.txt", sizes=F, pairs=TRUE, dataType="Diploid", verbose = TRUE)
+
+genedivFis(inputFile="Data/Oly2016NFH+2017NFW_Merged.txt", sizes=FALSE, outputFile = "Analyses/NF-Fis.txt", dataType = "Diploid", verbose=interactive())
